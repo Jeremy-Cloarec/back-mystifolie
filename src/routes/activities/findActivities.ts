5537ead16f6e9b11097e823841ed9d05218c6973
Jeremy-Cloarec <jeremycloarec@msn.com>
2024-03-10 20:16:00 +0100

    Find by pk OK

--- src/routes/activities/findActivities.ts
+++ src/routes/activities/findActivities.ts
@@ -1,17 +1,16 @@
-import { Activity } from '../../db/sequelize'
-
+import { Activity } from '../../db/sequelize';
 
 export default function findActivitie(app) {
     app.get('/mystifolie/activities/:id', async (req, res) => {
+
         try {
-            const activity = await Activity.findByPk(req.params.id);
-            console.log(activity); // Log the retrieved activities
-            res.json(activity)
+            const activity = await Activity.findByPk(req.params.id); // 
+            console.log(activity); // Log the retrieved activity
+            res.json(activity); // Renvoie l'activité sous forme de réponse JSON
 
         } catch (error) {
-            console.log('Oups ! Unable to retrieve activities from the database', error);
+            console.log('Oups ! Unable to retrieve activity from the database', error);
             res.status(500).send('Internal Server Error');
         }
-
     });
 }
