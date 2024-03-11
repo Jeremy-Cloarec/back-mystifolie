feb33d7250775156cb5d8bac62f561f378c1a0d9
Jeremy-Cloarec <jeremycloarec@msn.com>
2024-03-11 22:28:11 +0100

    Delete activity OK

--- src/routes/activities/deleteActivities.ts
+++ src/routes/activities/deleteActivities.ts
@@ -1,6 +1,22 @@
+import { Activity } from '../../db/sequelize';
 
 export default function deleteActivitie(app) {
-    app.delete('/mystifolie/activites/:id', (req, res) => {
-        res.send('Delete an activitie');
+    app.delete('/mystifolie/activites/:id', async (req, res) => {
+        try {
+            const activity = await Activity.findByPk(req.params.id)
+            if (!activity) {
+                return res.status(404).json({ message: 'Activity not found' });
+            }
+
+            await activity.destroy()
+
+            const message = `The activity has been destroyed`
+            res.json({ message })
+
+        } catch (error) {
+            console.log('Failed to delete activity from the database', error);
+            res.status(500).send('Internal Server Error');
+
+        }
     });
 }
--- src/routes/activities/findActivities.ts
+++ src/routes/activities/findActivities.ts
@@ -5,9 +5,9 @@ export default function findActivitie(app) {
 
         try {
             const activity = await Activity.findByPk(req.params.id)
-            const message = 'Une activité a bien été trouvée'
+            const message = 'An activity has been found !'
             res.json({ message, data: activity })
-           
+
         } catch (error) {
             console.log('Oups ! Unable to retrieve activity from the database', error);
             res.status(500).send('Internal Server Error');
