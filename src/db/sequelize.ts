d9f53f5db697990cdfc8deb651b067b928407701
Jeremy-Cloarec <jeremycloarec@msn.com>
2024-04-24 23:14:40 +0200

    define database for production

--- src/db/sequelize.ts
+++ src/db/sequelize.ts
@@ -1,22 +1,43 @@
 
 const { Sequelize, DataTypes } = require('sequelize')
-import {dataActivitie} from '../json/dataActivitie'
+import { dataActivitie } from '../json/dataActivitie'
 import { ActivityModel } from '../models/activity';
 
-// Make a new connexion
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: 'localhost',
-        dialect: 'mysql',
-        dialectOptions: {
-            timezone: '+02:00'
-        },
-        logging: false
-    }
-)
+let sequelize: { authenticate: () => any; sync: (arg0: { force: boolean; }) => any; };
+
+if (process.env.NODE_ENV === 'production') {
+    // Make a new connexion
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            dialect: 'mysql',
+            dialectOptions: {
+                timezone: '+02:00'
+            },
+            logging: true
+        }
+    )
+} else {
+    // Make a new connexion
+    sequelize = new Sequelize(
+        "mystifolie",
+        "root",
+        "",
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            dialectOptions: {
+                timezone: '+02:00'
+            },
+            logging: false
+        }
+    )
+}
+
+
 
 const activitieData = dataActivitie
 
@@ -35,7 +56,7 @@ export const Activity = ActivityModel(sequelize, DataTypes);
 //Synchronize model with db
 export async function SynchroniseDB() {
     try {
-        await sequelize.sync({force:true})
+        await sequelize.sync({ force: true })
         await Activity.bulkCreate(activitieData);
 
         console.log('Yeah ! Success to synchronize database');
