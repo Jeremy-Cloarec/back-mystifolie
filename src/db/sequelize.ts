6805d4ccdb92883755898c2500c2639f4797177b
Jeremy-Cloarec <jeremycloarec@msn.com>
2024-04-24 23:53:33 +0200

    fix bug for data production

--- src/db/sequelize.ts
+++ src/db/sequelize.ts
@@ -8,9 +8,9 @@ let sequelize;
 
 if (process.env.NODE_ENV === 'production') {
     // Make a new connexion
-    sequelize = new Sequelize('rznou9wwmzlmfy83', 'liun3e423h6agphh', 'nlhbmnpydbe2h4rz',
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,
         {
-            host: 'erxv1bzckceve5lh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+            host: process.env.DB_HOST,
             dialect: 'mysql',
             dialectOptions: {
                 timezone: '+02:00'
