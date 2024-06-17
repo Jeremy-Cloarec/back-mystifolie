c4fd5b803a41606b8738ac79c740321a0dc898e6
Jeremy Cloarec <jeremycloarec@msn.com>
2024-06-17 23:48:46 +0200

    JWT token

--- src/routers/activity.router.ts
+++ src/routers/activity.router.ts
@@ -1,18 +1,13 @@
 import { Router } from "express";
 import { ActivityController } from '../controllers/activity.controller'
-
-const authenticateJWT = require('../middlewares/auth.middlewares')
+import {authenticateJWT} from '../middlewares/auth.middlewares' 
 
 const router = Router();
 
-router.get('/', ActivityController.getAll)
-router.get('/:id', ActivityController.get)
-router.post('/', ActivityController.post)
-router.put('/:id', ActivityController.put)
-router.delete('/:id', ActivityController.delete)
-
-router.get('/protected-route', authenticateJWT, (req, res) => {
-    res.send('This is a protected route');
-});
+router.get('/', authenticateJWT, ActivityController.getAll)
+router.get('/:id', authenticateJWT, ActivityController.get)
+router.post('/', authenticateJWT,ActivityController.post)
+router.put('/:id', authenticateJWT, ActivityController.put)
+router.delete('/:id',authenticateJWT, ActivityController.delete)
 
 export default router
